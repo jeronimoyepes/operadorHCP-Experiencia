b907f8439e0244d8cec920146bc71ce823e23049
Jeronimo <jeronimo_y_g@hotmail.com>
2022-10-23 19:46:34 -0500

    Pages in instructions
    Signed-off-by: Jeronimo <jeronimo_y_g@hotmail.com>

--- components/instructions/Pagerenderer.jsx
+++ components/instructions/Pagerenderer.jsx
@@ -12,4 +12,4 @@ export default function PageRenderer({ children, pagesAmount }) {
       </div>
     </div>
   );
-}
+}  
--- pages/instructions/index.jsx
+++ pages/instructions/index.jsx
@@ -7,59 +7,57 @@ import { Layout } from "@/components/layout/Layout";
 import styles from "./instructions.module.scss";
 
 // Instruction pages
-import Page0 from "@/components/instructions/instructionPage/Page0";
-import Page1 from "@/components/instructions/instructionPage/Page1";
-import Page2 from "@/components/instructions/instructionPage/Page2";
-import Page3 from "@/components/instructions/instructionPage/Page3";
+// import Page0 from "@/components/instructions/instructionPage/Page0";
+// import Page1 from "@/components/instructions/instructionPage/Page1";
+// import Page2 from "@/components/instructions/instructionPage/Page2";
+// import Page3 from "@/components/instructions/instructionPage/Page3";
 
 const Instructions = () => {
   const [instructionPage, setInstructionPage] = useState(0);
-  const [currentPage, setcurrentPage] = useState(<Page0 />);
+  // const [currentPage, setcurrentPage] = useState(<Page0 />);
 
-  const pages = [
-    <Page0 key="0" />,
-    <Page1 key="1" />,
-    <Page2 key="2" />,
-    <Page3 key="3" />,
-  ];
+  // const pages = [
+  //   <Page0 key="0" />,
+  //   <Page1 key="1" />,
+  //   <Page2 key="2" />,
+  //   <Page3 key="3" />,
+  // ];
 
   useEffect(() => {
-    document.addEventListener("keyup", function (event) {
-      const key = event.key;
-      if (key == "ArrowRight") {
-        instructionPage == pages.length - 1
-          ? null
-          : setInstructionPage(instructionPage + 1);
+    function handleKeyDown(e) {
+      if (e.key == "ArrowRight") {
+        setInstructionPage(instructionPage + 1);
+        return;
       }
-      if (key == "ArrowLeft") {
-        instructionPage == 0 ? null : setInstructionPage(instructionPage - 1);
+      if (e.key == "ArrowLeft") {
+        setInstructionPage(instructionPage - 1);
+        return;
       }
-      console.log(instructionPage);
-    });
+    }
+    window.addEventListener("keyup", handleKeyDown);
+
+    return () => {
+      window.removeEventListener("keyup", console.log("finished"));
+    };
   }, []);
 
   // useEffect(() => {
-  //   switch (instructionPage) {
-  //     case 0:
-  //       setCurrentComponent(<Page0 />);
-  //       break;
-  //     case 1:
-  //       setCurrentComponent(<Page1 />);
-  //       break;
-  //     case 2:
-  //       setCurrentComponent(<Page2 />);
-  //       break;
-  //     case 3:
-  //       setCurrentComponent(<Page3 />);
-  //       break;
-  //   }
+  //   console.log(instructionPage);
+  //   // switch (instructionPage) {
+  //   //   case 0:
+  //   //     setcurrentPage(<Page0 />);
+  //   //     break;
+  //   //   case 1:
+  //   //     setcurrentPage(<Page1 />);
+  //   //     break;
+  //   //   case 2:
+  //   //     setcurrentPage(<Page2 />);
+  //   //     break;
+  //   //   case 3:
+  //   //     setcurrentPage(<Page3 />);
+  //   //     break;
+  //   // }
   // }, [instructionPage]);
-
-  const getComponent = () => {
-    let component;
-
-    return component;
-  };
 
   const asideGeneralData = {
     h1: "IC-HCP",
@@ -73,11 +71,13 @@ const Instructions = () => {
       <div className={styles.container}>
         <AsideGeneral props={asideGeneralData}></AsideGeneral>
         <div className={styles.pages}>
-          {currentPage}
+          {/* {currentPage} */}
+          {console.log(instructionPage)}
           <div>
             {/* {pagesAmount.map((page) => {
               return <a href="">a</a>;
             })} */}
+            asdadsfas fas asd fasd f
           </div>
         </div>
       </div>
