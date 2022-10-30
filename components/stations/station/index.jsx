67dc34c6093a2c05f4bdccb40205d51ee7e52ddc
Jeronimo <jeronimo_y_g@hotmail.com>
2022-10-30 10:56:22 -0500

    Modularize station component
    Signed-off-by: Jeronimo <jeronimo_y_g@hotmail.com>

---
+++ components/stations/station/index.jsx
@@ -0,0 +1,56 @@
+import DecisionsInteraction from "../../interactions/decisionsInteraction";
+import styles from "./artic.module.scss";
+import { useContext, useEffect, useState } from "react";
+import { Context } from "pages/mainExperience";
+import getTimeForEachInteraction from "../helpers/getTimeForEachInteraction";
+import SequenceInteraction from "@/components/interactions/sequenceInteraction";
+
+export default function Station({interactionData}) {
+  const context = useContext(Context);
+
+  const [currentInteractionData, setCurrentInteractionData] = useState();
+
+  const interactionTime = getTimeForEachInteraction(
+    context.experienceTotalDuration,
+    interactionData.length
+  );
+
+  useEffect(() => {
+    // Retornar la interacción cuyo rango de tiempo contenga el tiempo transcurrido de la experiencia
+    interactionData.map((interaction) => {
+      const interactionTimeLimit =
+        (interactionData.indexOf(interaction) + 1) * interactionTime;
+      // Si el tiempo transcurrido está dentro del rango de la interacción
+      if (
+        context.timeElapsed < interactionTimeLimit &&
+        context.timeElapsed >= interactionTimeLimit - interactionTime
+      ) {
+        return setCurrentInteractionData(interaction);
+      }
+    });
+  }, [context.timeElapsed]);
+
+  return (
+    <div className={styles.container}>
+      {currentInteractionData && (
+        <>
+          <div className={styles.header}>
+            {currentInteractionData.title}
+            <img src={`reportType-${currentInteractionData.type}.png`} alt="" />
+          </div>
+          <div className={styles.body}>{currentInteractionData.body}</div>
+          <div className={styles.sequence}>
+            {currentInteractionData.sequence && (
+              <SequenceInteraction sequence={currentInteractionData.sequence} />
+            )}
+          </div>
+          {currentInteractionData.actions && (
+            <DecisionsInteraction
+              currentInteractionData={currentInteractionData}
+            />
+          )}
+        </>
+      )}
+    </div>
+  );
+}
