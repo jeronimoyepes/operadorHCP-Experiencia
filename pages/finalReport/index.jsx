4de212373421d878ca70ea656a79535d75faaba8
Jeronimo <jeronimo_y_g@hotmail.com>
2022-10-30 21:39:03 -0500

    start final reports page
    Signed-off-by: Jeronimo <jeronimo_y_g@hotmail.com>

--- pages/finalReport/index.jsx
+++ pages/finalReport/index.jsx
@@ -1,10 +1,28 @@
 import { Layout } from "@/components/layout/Layout";
-import styles from "./finalReport.module.scss"
+import styles from "./finalReport.module.scss";
 
 export default function FinalReport() {
   return (
     <Layout title={"Reporte"}>
-      <div>Reporte Final</div>
+      <div className={styles.header}>
+        <h2>Reporte de rendimiento</h2>
+        <div>
+          <p>Gracias por tu valioso tiempo operados. HCP.</p>
+          <img src="warn-lines.svg" alt="" />
+        </div>
+      </div>
+      <div className={styles.timelines}>
+        <p>Estaciones</p>
+        <div>
+          <div className={styles.label}>
+            <img src="station-lunar.svg" alt="Lunar" />
+            <p>Lunar</p>
+          </div>
+          <div className={styles.line}>
+            
+          </div>
+        </div>
+      </div>
     </Layout>
   );
 }
